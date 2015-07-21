8f2edb3e1a45aeec2c7081aca245b8499c165f02
Ari Kobren <akobren@cs.umass.edu>
2015-07-21 11:11:33 -0400

    change distorExp to save results to file

--- distortExpr.py
+++ distortExpr.py
@@ -1,98 +1,88 @@
 import argparse
-import numpy as np
+import datetime
 import math
+import numpy as np
+import os
 
 from TotAffMatcher import TotAffMatcher
 from FairMatcher import FairMatcher
 from DistortionMatcher import DistortionMatcher
-from matplotlib import pyplot as plt
 
 import weights as wgts
 
-def runDistortionExperiment(n_rev, n_pap, alpha, beta, itrs, n_exps, verbose=False, w_samp=None, constr_per_itr=1, bp1=2, bp2=2, matcher='affinity'):
+def createDir(dir_name):
+    try:
+        os.makedirs(dir_name)
+    except OSError, e:
+        if e.errno != 17:
+            raise # This was not a "directory exist" error..
+
+
+def runDistortionExperiment(n_rev, n_pap, alpha, beta, itrs, outdir, verbose=False, w_samp=None, constr_per_itr=1, bp1=2, bp2=2, matcher='affinity'):
     all_diffs = []
     all_objectives = []
     all_affs = []
 
-    for e in range(n_exps):
-        print "Running Experiment: " + str(e)
+    # draw a new set of weights
+    if w_samp == 'beta':
+        weights = wgts.fromBeta(n_rev, n_pap, bp1, bp2)
+    elif w_samp == 'per_rev':
+        weights = wgts.skillBased(n_rev, n_pap, bp1, bp2)
+    elif w_samp == 'uni':
+        weights = wgts.fromUni(n_rev, n_pap)
+    else:
+        pass #weights from file
 
-        # draw a new set of weights
-        if w_samp == 'beta':
-            weights = wgts.fromBeta(n_rev, n_pap, bp1, bp2)
-        elif w_samp == 'per_rev':
-            weights = wgts.skillBased(n_rev, n_pap, bp1, bp2)
-        else:
-            weights = wgts.fromUni(n_rev, n_pap)
+    # sample new constraints
+    pairs = [ (i,j) for i in range(n_rev) for j in range(n_pap) ]
+    arbitraryConsts = np.random.choice(len(pairs), itrs * constr_per_itr, replace=False)
 
-        # print heat map of reviewer weights
-#        wgts.showWeights(weights)
+    # construct new problem instance and solve for initial solution
+    if matcher.lower() == 'makespan':
+        print "MAKESPAN"
+        prob = FairMatcher(n_rev, n_pap, alpha, beta, weights)
+    elif matcher.lower() == 'distortion':
+        print "Selected distortion"
+        prob = DistortionMatcher(n_rev, n_pap, alpha, beta, weights)
+    else:
+        prob = TotAffMatcher(n_rev, n_pap, alpha, beta, weights)
 
-        # sample new constraints
-        pairs = [ (i,j) for i in range(n_rev) for j in range(n_pap) ]
-        arbitraryConsts = np.random.choice(len(pairs), itrs * constr_per_itr, replace=False)
+    if verbose:
+        prob.turn_on_verbosity()
 
-        # construct new problem instance and solve for initial solution
-        if matcher.lower() == 'makespan':
-            print "MAKESPAN"
-            prob = FairMatcher(n_rev, n_pap, alpha, beta, weights)
-        elif matcher.lower() == 'distortion':
-            print "Selected distortion"
-            prob = DistortionMatcher(n_rev, n_pap, alpha, beta, weights)
-        else:
-            prob = TotAffMatcher(n_rev, n_pap, alpha, beta, weights)
+    prob.solve()
 
-        if verbose:
-            prob.turn_on_verbosity()
+    n_diffs = []
+    objectives = []
+
+    # add in each of the new constraints (1 by 1 for now) and resolve the problem
+    for i in range(0, itrs):
+        for j in range(constr_per_itr):
+            print "\tAdding constraint: " + str(i * constr_per_itr + j)
+            (next_i, next_j) = pairs[arbitraryConsts[i * constr_per_itr + j]]
+            prob.add_hard_const(next_i, next_j)
         prob.solve()
+        objectives.append(prob.objective_val())
 
-        n_diffs = []
-        objectives = []
+        # calculate the number of variables that changed between the current and previous sols
+        n_diffs.append(prob.num_diffs(prob.prev_sols[-1], prob.prev_sols[-2]))
 
-        # add in each of the new constraints (1 by 1 for now) and resolve the problem
-        for i in range(0, itrs):
-            for j in range(constr_per_itr):
-                print "\tAdding constraint: " + str(i * constr_per_itr + j)
-                (next_i, next_j) = pairs[arbitraryConsts[i * constr_per_itr + j]]
-                prob.add_hard_const(next_i, next_j)
-            prob.solve()
-            objectives.append(prob.objective_val())
+    # bookkeeping
+    all_diffs.append(n_diffs)
+    all_objectives.append(objectives)
+    all_affs.append(prob.prev_affs[-1].reshape(-1))
 
-            # calculate the number of variables that changed between the current and previous sols
-            n_diffs.append(prob.num_diffs(prob.prev_sols[-1], prob.prev_sols[-2]))
+    # save data to csv
+    createDir(outdir + "/diffs")
+    createDir(outdir + "/objs")
+    createDir(outdir + "/affs")
 
-        # bookkeeping
-        all_diffs.append(n_diffs)
-        all_objectives.append(objectives)
-        all_affs.append(prob.prev_affs[-1].reshape(-1))
+    now = datetime.datetime.now()
+    exec_time = now.strftime("%Y%m%d_%H%M%S%f")
+    np.savetxt(outdir + "/diffs/" + exec_time + "-diffs.csv", all_diffs, delimiter=',')
+    np.savetxt(outdir + "/affs/" + exec_time + "-affs.csv", all_affs, delimiter=',')
+    np.savetxt(outdir + "/objs/" + exec_time + "-objs.csv", all_objectives, delimiter=',')
 
-    mean_diffs = np.mean(np.array(all_diffs), 0)
-    std_diffs = np.std(np.array(all_diffs),0)
-    mean_objs = np.mean(np.array(all_objectives), 0)
-    std_objs = np.std(np.array(all_objectives), 0)
-    mean_affs = np.mean(np.array(all_affs), 0)
-    std_affs = np.std(np.array(all_affs),0)
-
-    plt.figure(1)
-    plt.subplot(311)
-    plt.errorbar(range(len(mean_diffs)), mean_diffs, yerr=std_diffs)
-    plt.title("REVIEWERS: " + str(n_rev) + "; PAPERS: " + str(n_pap) + "; ALPHA: " +
-              str(alpha) + "; BETA: " + str(beta))
-    plt.ylabel("Num. Diff. Assignments")
-
-    plt.subplot(312)
-    plt.errorbar(range(len(mean_objs)), mean_objs, yerr=std_objs)
-    plt.xlabel("Iteration")
-    plt.ylabel("Objective Value")
-
-    plt.subplot(313)
-#    ind = np.arange(len(mean_affs))
-#    width = 0.2
-#    plt.bar(ind + width, mean_affs, width, yerr=std_affs)
-    plt.hist(mean_affs, bins=alpha * 20)
-    plt.xlabel("Reviewer Affinity")
-    plt.ylabel("# of Sols")
-    plt.show()
 
 if __name__ == "__main__":
     beta_param1 = 0.5
@@ -102,7 +92,7 @@ if __name__ == "__main__":
     parser.add_argument('reviewers', type=int, help='the number of reviewers')
     parser.add_argument('papers', type=int, help='the number of papers')
     parser.add_argument('reviews_per_paper', type=int, help='the number of reviews per paper')
-    parser.add_argument('experiments', type=int, help='the number of experiment repetitions')
+    parser.add_argument('outdir', type=str, help='the directory into which to write the results')
     parser.add_argument('itrs', type=int, help='the number of iterations to run')
     parser.add_argument('consts_per_itr', type=int, help='the number of constraints to add each iteration')
     parser.add_argument('matcher', type=str, help='either affinity (default), makespan or two-phase')
@@ -113,14 +103,14 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    # PARAMETERS (inspired by EMNLP)
+    # PARAMETERS (inspired by EMNLP 2015)
     n_rev = args.reviewers
     n_pap = args.papers
     beta = args.reviews_per_paper
     alpha = math.ceil((n_pap * beta) / float(n_rev))    # reviwer cannot review > alpha
     itrs = args.itrs
-    n_exps = args.experiments
+    outdir = args.outdir
     w_samp = 'beta' if args.beta else None
     w_samp = 'per_rev' if args.per_reviewer else w_samp
 
-    runDistortionExperiment(n_rev, n_pap, alpha, beta, itrs, n_exps, args.verbose, w_samp, args.consts_per_itr, beta_param1, beta_param2, args.matcher)
+    runDistortionExperiment(n_rev, n_pap, alpha, beta, itrs, outdir, args.verbose, w_samp, args.consts_per_itr, beta_param1, beta_param2, args.matcher)
