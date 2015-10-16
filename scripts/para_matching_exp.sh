60bb5d15cb137464bb0b2abfd56c8d9effc9efe2
Ari Kobren <akobren@cs.umass.edu>
2015-10-16 02:22:07 -0400

    renamed file and fixed it up so it works

--- scripts/para_matching_exp.sh
+++ scripts/para_matching_exp.sh
@@ -19,27 +19,33 @@ EXP_NAMES=()
 # usage: distortExpr.py [-h] [-v] [-b] [-p] [-f [WEIGHTS_FILE]]
 #                       reviewers papers reviews_per_paper itrs consts_per_itr
 #                       matcher
-EXPERIMENTS+=("500 900 3 6 'relaxed' -f ./weights/exp1-500-900-0.5-2.0-skill_based")
-EXPERIMENTS+=("500 900 3 6 'relaxed' -f ./weights/exp2-500-900-0.5-2.0-skill_based")
-EXPERIMENTS+=("500 900 3 6 'relaxed' -f ./weights/exp3-500-900-0.5-2.0-skill_based")
-EXPERIMENTS+=("500 900 3 6 'relaxed' -f ./weights/exp4-500-900-0.5-2.0-skill_based")
-EXPERIMENTS+=("500 900 3 6 'relaxed' -f ./weights/exp5-500-900-0.5-2.0-skill_based")
+EXPERIMENTS+=("500 900 3 6 'relaxed' -f ./weights/exp1-500-900-0.5-2.0-skill_based.out")
+EXPERIMENTS+=("500 900 3 6 'relaxed' -f ./weights/exp2-500-900-0.5-2.0-skill_based.out")
+EXPERIMENTS+=("500 900 3 6 'relaxed' -f ./weights/exp3-500-900-0.5-2.0-skill_based.out")
+EXPERIMENTS+=("500 900 3 6 'relaxed' -f ./weights/exp4-500-900-0.5-2.0-skill_based.out")
+EXPERIMENTS+=("500 900 3 6 'relaxed' -f ./weights/exp5-500-900-0.5-2.0-skill_based.out")
 
-EXPERIMENTS+=("500 900 3 6  -f ./weights/exp1-500-900-0.5-2.0-skill_based")
-EXPERIMENTS+=("500 900 3 6  -f ./weights/exp2-500-900-0.5-2.0-skill_based")
-EXPERIMENTS+=("500 900 3 6  -f ./weights/exp3-500-900-0.5-2.0-skill_based")
-EXPERIMENTS+=("500 900 3 6  -f ./weights/exp4-500-900-0.5-2.0-skill_based")
-EXPERIMENTS+=("500 900 3 6  -f ./weights/exp5-500-900-0.5-2.0-skill_based")
+EXPERIMENTS+=("500 900 3 6 'affinity' -f ./weights/exp1-500-900-0.5-2.0-skill_based.out")
+EXPERIMENTS+=("500 900 3 6 'affinity' -f ./weights/exp2-500-900-0.5-2.0-skill_based.out")
+EXPERIMENTS+=("500 900 3 6 'affinity' -f ./weights/exp3-500-900-0.5-2.0-skill_based.out")
+EXPERIMENTS+=("500 900 3 6 'affinity' -f ./weights/exp4-500-900-0.5-2.0-skill_based.out")
+EXPERIMENTS+=("500 900 3 6 'affinity' -f ./weights/exp5-500-900-0.5-2.0-skill_based.out")
 
-EXP_NAMES+=("00-relaxed-500-900")
-EXP_NAMES+=("01-relaxed-500-900")
-EXP_NAMES+=("02-relaxed-500-900")
-EXP_NAMES+=("03-relaxed-500-900")
-EXP_NAMES+=("04-relaxed-500-900")
+EXP_NAMES+=("exp1-relaxed-500-900")
+EXP_NAMES+=("exp2-relaxed-500-900")
+EXP_NAMES+=("exp3-relaxed-500-900")
+EXP_NAMES+=("exp4-relaxed-500-900")
+EXP_NAMES+=("exp5-relaxed-500-900")
+
+EXP_NAMES+=("exp1-affinity-500-900")
+EXP_NAMES+=("exp2-affinity-500-900")
+EXP_NAMES+=("exp3-affinity-500-900")
+EXP_NAMES+=("exp4-affinity-500-900")
+EXP_NAMES+=("exp5-affinity-500-900")
 
 for ((i = 0; i < ${#EXPERIMENTS[@]}; i++))
 do
-    echo "/share/apps/python-2.7.10/bin/python2.7 ${EXPERIMENT_FILE} ${EXPERIMENTS[$i]}" | qsub -V -cwd  -j y -o ${OUTDIR}/${EXP_NAMES[$i]}".log" -l mem_token=10G  -N "relaxed-paper-matching"  -S /bin/sh
+    echo "/share/apps/python-2.7.10/bin/python2.7 ${EXPERIMENT_FILE} ${EXPERIMENTS[$i]}" | qsub -V -cwd  -j y -o ${OUTDIR}/${EXP_NAMES[$i]}".log" -l mem_token=10G  -N ${EXP_NAMES[$i]}  -S /bin/sh
 done
 
 echo "[done.]"
