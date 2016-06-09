338fe4267b8758aca5a6abda9dff2a7d3b15ee17
Ari Kobren <akobren@cs.umass.edu>
2016-06-09 17:26:19 -0400

    added some options/commented out some code to generate the different matrices from kou et al

--- src/utils/gen_kou_mats.py
+++ src/utils/gen_kou_mats.py
@@ -13,8 +13,10 @@ def read_pap_profile(f, n_tops, pre):
 
 if __name__ == "__main__":
     data_dir = "/Users/akobren/software/repos/git/lp-ir/data/kou_et_al"
-    dbs = ["DataMining08", "DataMining09", "Databases08", "Databases09", "Theory08", "Theory09"]
+    # dbs = ["DataMining08", "DataMining09", "Databases08", "Databases09", "Theory08", "Theory09"]
     # dbs = ["DataMining08", "Databases08"]
+    dbs = ["DataMining08"]
+    # dbs = ["Databases08"]
     papers = "paper-profile"
     reviewers = "reviewer-profile"
 
@@ -35,18 +37,26 @@ if __name__ == "__main__":
     for k,v in revs.iteritems():
         rev_mat.append(v)
 
-    print len(pap_mat)
-    print len(rev_mat)
     reviewer_tensor = np.tile(np.array(rev_mat)[:,np.newaxis,:], (1,len(pap_mat),1))
     paper_tensor = np.tile(np.array(pap_mat)[np.newaxis,:,:], (len(rev_mat),1,1))
     score_mat = np.sum(np.minimum(reviewer_tensor, paper_tensor), axis=2)
-    np.save("kou_pap_mat", np.array(pap_mat))
-    np.save("kou_rev_mat", np.array(rev_mat))
-    np.save("kou_pap_tensor", paper_tensor)
-    np.save("kou_rev_tensor", reviewer_tensor)
-    np.save("kou_score_mat", score_mat)
+    # np.save("kou_pap_mat", np.array(pap_mat))
+    # np.save("kou_rev_mat", np.array(rev_mat))
+    # np.save("kou_pap_tensor", paper_tensor)
+    # np.save("kou_rev_tensor", reviewer_tensor)
+    # np.save("kou_score_mat", score_mat)
     # np.save("kou_pap_mat_dbdm", np.array(pap_mat))
     # np.save("kou_rev_mat_dbdm", np.array(rev_mat))
     # np.save("kou_pap_tensor_dbdm", paper_tensor)
     # np.save("kou_rev_tensor_dbdm", reviewer_tensor)
     # np.save("kou_score_mat_dbdm", score_mat)
+    np.save("kou_pap_mat_dm08", np.array(pap_mat))
+    np.save("kou_rev_mat_dm08", np.array(rev_mat))
+    np.save("kou_pap_tensor_dm08", paper_tensor)
+    np.save("kou_rev_tensor_dm08", reviewer_tensor)
+    np.save("kou_score_mat_dm08", score_mat)
+    # np.save("kou_pap_mat_db08", np.array(pap_mat))
+    # np.save("kou_rev_mat_db08", np.array(rev_mat))
+    # np.save("kou_pap_tensor_db08", paper_tensor)
+    # np.save("kou_rev_tensor_db08", reviewer_tensor)
+    # np.save("kou_score_mat_db08", score_mat)
