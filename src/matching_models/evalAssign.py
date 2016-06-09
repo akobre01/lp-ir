c2ce4dd39806a1a192b261b685f4ea376ef8fd9a
Ari Kobren <akobren@cs.umass.edu>
2016-06-09 17:24:49 -0400

    some code to run WGRAP objective baseline, compute states and plot some of the paper scores

--- src/matching_models/evalAssign.py
+++ src/matching_models/evalAssign.py
@@ -3,9 +3,11 @@ from WGRAP import WGRAP
 from matplotlib import pyplot as plt
 
 if __name__ == "__main__":
-    rev_mat_file = "/Users/akobren/software/repos/git/lp-ir/data/train/kou_et_al/kou_rev_mat_dbdm.npy"
-    pap_mat_file = "/Users/akobren/software/repos/git/lp-ir/data/train/kou_et_al/kou_pap_mat_dbdm.npy"
-    beta = 3
+    dataset = "db08"
+    alg = "wgrap" # bb, da, wgrap
+    rev_mat_file = "/Users/akobren/software/repos/git/lp-ir/data/train/kou_et_al/kou_rev_mat_%s.npy" % dataset
+    pap_mat_file = "/Users/akobren/software/repos/git/lp-ir/data/train/kou_et_al/kou_pap_mat_%s.npy" % dataset
+    beta = 3.0
     rev_mat = np.load(rev_mat_file)
     pap_mat = np.load(pap_mat_file)
     num_rev = np.size(rev_mat, axis=0)
@@ -13,7 +15,8 @@ if __name__ == "__main__":
     alpha = np.ceil(num_pap * beta / num_rev)
     wgrap = WGRAP(rev_mat, pap_mat, alpha, beta)
 
-    out_file = "../../data/train/kou_et_al/wgrap-dbdm-%d-%d-assign-refine-9" % (alpha, beta)
+    # out_file = "../../data/train/kou_et_al/%s-%s-%d-%d-assign-refine-9" % (alg, dataset, alpha, beta)
+    out_file = "../../data/train/kou_et_al/bb-kou_score_mat_db08-seqmkspn-18.0-3.assignment-1.269843"
     assign_mat = np.load(out_file + ".npy")
     wgrap.curr_assignment = assign_mat
     print wgrap.score_assignment()
@@ -31,25 +34,25 @@ if __name__ == "__main__":
             topic_scores.append(topic)
 
     plt.hist(paper_scores, bins=50)
-    plt.title('WGRAP Assignment Quality')
+    plt.title('%s: WGRAP Assignment Quality' % dataset)
     plt.xlabel('Group Score')
     plt.ylabel('# of Papers')
     plt.show()
 
     plt.hist(filter(lambda x: x < 1.0, paper_scores), bins=50)
-    plt.title('WGRAP Assignment Quality (scores < 1.0)')
+    plt.title('%s: WGRAP Assignment Quality (scores < 1.0)' % dataset)
     plt.xlabel('Group Score')
     plt.ylabel('# of Papers')
     plt.show()
 
     plt.hist(topic_scores, bins=50)
-    plt.title('WGRAP Assignment Quality (by Topic)')
+    plt.title('%s: WGRAP Assignment Quality (by Topic)' % dataset)
     plt.xlabel('Coverage (max 1.0)')
     plt.ylabel('# of Topics')
     plt.show()
 
     plt.hist(filter(lambda x: x < 1.0, topic_scores), bins=50)
-    plt.title('WGRAP Assignment Quality (by Topic)')
+    plt.title('%s: WGRAP Assignment Quality (by Topic)' % dataset)
     plt.xlabel('Coverage (max 1.0)')
     plt.ylabel('# of Topics')
     plt.show()
--- src/matching_models/testWGRAP.py
+++ src/matching_models/testWGRAP.py
@@ -4,15 +4,15 @@ from WGRAP import WGRAP
 import time
 
 if __name__ == "__main__":
-    rev_mat_file = "/Users/akobren/software/repos/git/lp-ir/data/train/kou_et_al/kou_rev_mat_dbdm.npy"
-    pap_mat_file = "/Users/akobren/software/repos/git/lp-ir/data/train/kou_et_al/kou_pap_mat_dbdm.npy"
+    rev_mat_file = "/Users/akobren/software/repos/git/lp-ir/data/train/kou_et_al/kou_rev_mat_dm08.npy"
+    pap_mat_file = "/Users/akobren/software/repos/git/lp-ir/data/train/kou_et_al/kou_pap_mat_dm08.npy"
     beta = 3
     rev_mat = np.load(rev_mat_file)
     pap_mat = np.load(pap_mat_file)
     num_rev = np.size(rev_mat, axis=0)
     num_pap = np.size(pap_mat, axis=0)
     alpha = np.ceil(num_pap * beta / num_rev)
-    out_file = "../../data/train/kou_et_al/wgrap-dbdm-%d-%d-assign" % (alpha, beta)
+    out_file = "../../data/train/kou_et_al/wgrap-dm08-%d-%d-assign" % (alpha, beta)
 
     print "REV MAT SHAPE: %s" % str(rev_mat.shape)
     print "PAP MAT SHAPE: %s" % str(pap_mat.shape)
