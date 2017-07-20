69bad9f61175ffeb4df25a1232e23c201e8deee1
Ari Kobren <akobren@cs.umass.edu>
2017-07-20 17:05:21 -0400

    added run script for mmir

---
+++ src/exps/run_mmir.py
@@ -0,0 +1,57 @@
+"""Run makespan formulation (using gurobi's ILP internal techniques)."""
+import argparse
+import numpy as np
+import os
+import time
+
+from ..matching_models.IRMakespanMatcher import IRMakespanMatcher
+
+if __name__ == "__main__":
+    """Solve the makespan version of paper matching using iterative relaxation.
+
+    If no makespan is passed in, run a binary search over possible makespan
+    values to find the best value.  If a makespan is passed in, then only run
+    using that makespan.
+
+    This script writes the assignment constructed as well as the total time to
+    construct the assignment.
+    """
+    parser = argparse.ArgumentParser(
+        description='Run iterative relaxation solver on dataset.')
+    parser.add_argument('cov_const', type=int, help='# of reviewers per paper')
+    parser.add_argument('weight_file', type=str,
+                        help='the file from which to read the weights')
+    parser.add_argument('output', type=str,
+                        help='the directory in which to save the results.')
+    parser.add_argument('-m', '--makespan', type=float,
+                        help='the value of the makespan to run.')
+    args = parser.parse_args()
+
+    coverage = args.cov_const
+    weights = np.load(args.weight_file)
+    weights_name = args.weight_file[
+                   args.weight_file.rfind('/') + 1:args.weight_file.rfind('.')]
+    n_rev = np.size(weights, axis=0)
+    n_pap = np.size(weights, axis=1)
+
+    max_load = np.ceil(n_pap * float(coverage) / n_rev)
+    out_dir = args.output
+    if not os.path.isdir(out_dir):
+        os.makedirs(out_dir)
+
+    # Output files.
+    assignment_file = os.path.join(args.output, 'assignment')
+    time_file = os.path.join(args.output, 'time.tsv')
+
+    ir = IRMakespanMatcher([max_load] * n_rev, [coverage] * n_pap, weights)
+    s = time.time()
+    if args.makespan:
+        ir.change_makespan(args.makespan)
+        ir.solve_with_current_makespan()
+    else:
+        ir.solve()
+    t = time.time() - s
+    f = open(time_file, 'w')
+    f.write(str(t))
+    f.close()
+    np.save(assignment_file, ir.sol_as_mat())
