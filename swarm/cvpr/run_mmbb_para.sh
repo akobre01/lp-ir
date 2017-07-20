aed6a55664593b2c1d621b9f727e29c547a592d1
Ari Kobren <akobren@cs.umass.edu>
2017-07-20 15:20:17 -0400

    increased number of bb makespan values we're exploring

--- swarm/cvpr/run_mmbb_para.sh
+++ swarm/cvpr/run_mmbb_para.sh
@@ -2,14 +2,12 @@
 
 source `pwd`/swarm/setup.sh
 
-set -exu
-
 DATA_NAME="cvpr17acs-0.9-pow-0.9"
 DATASET="data/cvpr/cvpr17acs-0.9-pow-0.9.npy"
 COVERAGE=3
 ALG="bb"
 
-for ms in `seq 0 1 3`
+for ms in `seq 0 0.25 3`
 do 
     OUTDIR="results/${DATA_NAME}-cov=${COVERAGE}/${ALG}-ms=${ms}/"
 
