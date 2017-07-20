36c3ca32a80483e099ded4dcd9e5b1443ec58c34
Ari Kobren <akobren@cs.umass.edu>
2017-07-19 22:32:15 -0400

    setup script for swarm

---
+++ swarm/setup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+export PM_ROOT=`pwd`
+export PM_DATA_ROOT=$PM_ROOT/data
+export PYTHONPATH=$PM_ROOT/src:$PYTHONPATH
+
+if [ ! -f $PM_ROOT/.gitignore ]; then
+    echo ".gitignore" > $PM_ROOT/.gitignore
+    echo "target" >> $PM_ROOT/.gitignore
+    echo ".idea" >> $PM_ROOT/.gitignore
+    echo "__pycache__" >> $PM_ROOT/.gitignore
+fi
+
+# Setup log dir
+mkdir logs
+mkdir logs/cvpr
