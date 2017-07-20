c0889e5e3a5df19601c69a9ee3f476d17c2d150c
Ari Kobren <akobren@cs.umass.edu>
2017-07-20 11:13:50 -0400

    changed mem limit

--- swarm/cvpr/run_basic.sh
+++ swarm/cvpr/run_basic.sh
@@ -10,7 +10,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --time=0-01:00         # Runtime in D-HH:MM
-#SBATCH --mem=2048    # Memory in MB per cpu allocated
+#SBATCH --mem=4000    # Memory in MB per cpu allocated
 
 #source `pwd`/swarm/setup.sh
 set -exu
