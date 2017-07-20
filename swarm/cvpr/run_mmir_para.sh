7ca1c124a9acd0fdb44ade1726500b55efa05b00
Ari Kobren <akobren@cs.umass.edu>
2017-07-20 17:02:33 -0400

    swarm scripts for mmir

--- swarm/cvpr/run_mmir_para.sh
+++ swarm/cvpr/run_mmir_para.sh
@@ -5,16 +5,16 @@ source `pwd`/swarm/setup.sh
 DATA_NAME="cvpr17acs-0.9-pow-0.9"
 DATASET="data/cvpr/cvpr17acs-0.9-pow-0.9.npy"
 COVERAGE=3
-ALG="bb"
+ALG="ir"
 
 for ms in `seq 0 0.25 ${COVERAGE}`
-do 
+do
     OUTDIR="results/${DATA_NAME}-cov=${COVERAGE}/${ALG}-ms=${ms}/"
 
     # Create output director
     mkdir -p $OUTDIR
 
     # Run the basic lp formulation of paper matching.
-    sbatch $PM_ROOT/swarm/cvpr/run_mmbb_single.sh $COVERAGE $ms
+    sbatch $PM_ROOT/swarm/cvpr/run_mmir_single.sh $COVERAGE $ms
 done
 exit
---
+++ swarm/cvpr/run_mmir_single.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+#SBATCH --job-name=mmir_single
+#SBATCH --output=logs/cvpr/run_mmir_single.log
+#SBATCH -e logs/cvpr/run_mmir_single.err
+#SBATCH --partition=defq    # Partition to submit to
+#
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=0-02:00         # Runtime in D-HH:MM
+#SBATCH --mem=8000    # Memory in MB per cpu allocated
+
+source `pwd`/swarm/setup.sh
+
+set -exu
+
+DATA_NAME="cvpr17acs-0.9-pow-0.9"
+DATASET="data/cvpr/cvpr17acs-0.9-pow-0.9.npy"
+COVERAGE=$1
+ALG="ir"
+MS=$2
+OUTDIR="results/${DATA_NAME}-cov=${COVERAGE}/${ALG}-ms=${MS}/"
+
+# Create output director
+mkdir -p $OUTDIR
+
+# Run the basic lp formulation of paper matching.
+python -m src.exps.run_mmir $COVERAGE $DATASET $OUTDIR -m $MS
+
+exit
