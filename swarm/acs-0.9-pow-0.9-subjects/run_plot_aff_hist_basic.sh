8edcf012d4113b75cd95adbb8ec7bd09f10bcee7
Ari Kobren <akobren@cs.umass.edu>
2017-07-23 16:32:48 -0400

    added swarm directives to aff hist plotting script

--- swarm/acs-0.9-pow-0.9-subjects/run_plot_aff_hist_basic.sh
+++ swarm/acs-0.9-pow-0.9-subjects/run_plot_aff_hist_basic.sh
@@ -4,7 +4,7 @@ source `pwd`/swarm/setup.sh
 
 COVERAGE=3
 
-$INPUT=$1
+INPUT=$1
 
 # Run the basic lp formulation of paper matching.
 sbatch $PM_ROOT/bin/cvpr/run_plot_aff_hist.sh $INPUT $PM_DATA_ROOT/cvpr/acs-0.9-pow-0.9-subjects $COVERAGE
