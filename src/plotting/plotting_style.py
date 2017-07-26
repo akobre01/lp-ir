d84884550324f694cbc9aada6d21af7264f9eb06
Ari Kobren <akobren@cs.umass.edu>
2017-07-25 22:10:58 -0400

    tweaked colors

--- src/plotting/plotting_style.py
+++ src/plotting/plotting_style.py
@@ -1,8 +1,8 @@
 from collections import defaultdict
 
 MODEL_TO_COLOR = defaultdict(lambda: 'k')
-MODEL_TO_COLOR['ir'] = '#8dd3c7'
-MODEL_TO_COLOR['bb'] = '#bebada'
+MODEL_TO_COLOR['ir'] = '#bebada'
+MODEL_TO_COLOR['bb'] = '#8dd3c7'
 MODEL_TO_COLOR['basic'] = '#fb8072'
 MODEL_TO_COLOR['da'] = '#80b1d3'
 
