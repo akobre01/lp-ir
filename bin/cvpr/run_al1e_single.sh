cbd638e6d39163c7b2926d43495a5f8cf9097ffe
Ari Kobren <akobren@cs.umass.edu>
2017-07-25 23:15:31 -0400

    first cut at the at least 1 expert formulation, implemented very badly:

---
+++ bin/cvpr/run_al1e_single.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+#SBATCH --job-name=al1e_single
+#SBATCH --output=logs/cvpr/run_al1e_single.log
+#SBATCH -e logs/cvpr/run_al1e_single.err
+#SBATCH --partition=defq    # Partition to submit to
+#
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=0-02:00         # Runtime in D-HH:MM
+#SBATCH --mem=10000    # Memory in MB per cpu allocated
+
+set -exu
+
+DATA_NAME=$1
+DATASET=$2
+LOAD=$3
+COVERAGE=$4
+EXPERT=$5
+GAP=$6
+ALG="bb"
+OUTDIR="results/${DATA_NAME}-load=${LOAD}-cov=${COVERAGE}/${ALG}/expert=${EXPERT}-gap=${GAP}/"
+
+# Create output director
+mkdir -p $OUTDIR
+
+# Run the basic lp formulation of paper matching.
+python -m src.exps.run_al1e $LOAD $COVERAGE $DATASET $OUTDIR -m $EXPERT -g $GAP
+
+exit
---
+++ swarm/subjects-only/run_al1e_para.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+source `pwd`/swarm/setup.sh
+
+# This dataset has 1373 reviewers and 2623 papers.
+# if coverage is 3
+#   2623 * 3 / 1373 ~= 5.73
+# if coverage is 5
+#   2623 * 5 / 1373 ~= 9.55
+# if coverage is 7
+#   2623 * 7 / 1373 ~= 13.37
+# It is different from the other affinity matrices because it only uses
+# subject areas.
+
+DATA_NAME="subjects-only"
+DATASET="data/cvpr/subjects-only.npy"
+
+LOAD=17
+COVERAGE=7
+ALG="al1e"
+
+for gap in `seq 0.5 0.05 0.05`
+do
+    for expert in `seq 0 0.1 1.0`
+    do
+        # Run the basic lp formulation of paper matching.
+        sbatch $PM_ROOT/bin/cvpr/run_al1e_single.sh $DATA_NAME $DATASET $LOAD $COVERAGE $expert $gap
+    done
+done
+exit
