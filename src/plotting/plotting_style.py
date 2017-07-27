8a6cf1814eea5fa47ee9066a4c0c5442c910da74
Ari Kobren <akobren@cs.umass.edu>
2017-07-27 17:11:47 -0400

    updated colors again

--- src/plotting/plotting_style.py
+++ src/plotting/plotting_style.py
@@ -1,17 +1,17 @@
 from collections import defaultdict
 
 MODEL_TO_COLOR = defaultdict(lambda: 'k')
-MODEL_TO_COLOR['ir'] = '#7fc97f'
-MODEL_TO_COLOR['bb'] = '#beaed4'
-MODEL_TO_COLOR['basic'] = '#fdc086'
-MODEL_TO_COLOR['da'] = '#ffff99'
-MODEL_TO_COLOR['al1e'] = '#f0027f'
+MODEL_TO_COLOR['ir'] = '#66c2a5'
+MODEL_TO_COLOR['bb'] = '#fc8d62'
+MODEL_TO_COLOR['basic'] = '#8da0cb'
+MODEL_TO_COLOR['da'] = '#e78ac3'
+MODEL_TO_COLOR['al1e'] = '#a6d854'
 
-MODEL_TO_COLOR['irlb'] = '#7fc97f'
-MODEL_TO_COLOR['bblb'] = '#beaed4'
-MODEL_TO_COLOR['basic_lb'] = '#fdc086'
-MODEL_TO_COLOR['dalb'] = '#ffff99'
-MODEL_TO_COLOR['al1elb'] = '#f0027f'
+MODEL_TO_COLOR['irlb'] = '#66c2a5'
+MODEL_TO_COLOR['bblb'] = '#fc8d62'
+MODEL_TO_COLOR['basic_lb'] = '#8da0cb'
+MODEL_TO_COLOR['dalb'] = '#e78ac3'
+MODEL_TO_COLOR['al1elb'] = '#a6d854'
 
 BORDER_COLOR = 'lightgrey'
 LABEL_COLOR = 'k'
