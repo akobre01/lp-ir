7baa1f23de140087cc649f9350c949628a449313
Ari Kobren <akobren@cs.umass.edu>
2017-07-27 16:06:32 -0400

    survival plots for lb

--- swarm/subjects-only/run_plot_expert_survival.sh
+++ swarm/subjects-only/run_plot_expert_survival.sh
@@ -4,9 +4,10 @@ source `pwd`/swarm/setup.sh
 
 LOAD=11
 COV=5
+LB=7
 DATA_NAME="subjects-only"
 DATASET="data/cvpr/subjects-only.npy"
-ASSIGNMENTS="${PM_ROOT}/results/${DATA_NAME}-load=${LOAD}-cov=${COV}"
+ASSIGNMENTS="${PM_ROOT}/results/${DATA_NAME}-load=${LOAD}-cov=${COV}-loadlb=${LB}"
 
 # Run the basic lp formulation of paper matching.
 sbatch $PM_ROOT/bin/util/run_plot_expert_survival.sh $ASSIGNMENTS $DATASET
