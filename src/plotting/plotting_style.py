2c532e41106e8a65fbeeacce82c62f5071669e3f
Ari Kobren <akobren@cs.umass.edu>
2017-07-27 17:06:15 -0400

    updated colors

--- src/plotting/plotting_style.py
+++ src/plotting/plotting_style.py
@@ -1,10 +1,17 @@
 from collections import defaultdict
 
 MODEL_TO_COLOR = defaultdict(lambda: 'k')
-MODEL_TO_COLOR['ir'] = '#bebada'
-MODEL_TO_COLOR['bb'] = '#8dd3c7'
-MODEL_TO_COLOR['basic'] = '#fb8072'
-MODEL_TO_COLOR['da'] = '#80b1d3'
+MODEL_TO_COLOR['ir'] = '#7fc97f'
+MODEL_TO_COLOR['bb'] = '#beaed4'
+MODEL_TO_COLOR['basic'] = '#fdc086'
+MODEL_TO_COLOR['da'] = '#ffff99'
+MODEL_TO_COLOR['al1e'] = '#f0027f'
+
+MODEL_TO_COLOR['irlb'] = '#7fc97f'
+MODEL_TO_COLOR['bblb'] = '#beaed4'
+MODEL_TO_COLOR['basic_lb'] = '#fdc086'
+MODEL_TO_COLOR['dalb'] = '#ffff99'
+MODEL_TO_COLOR['al1elb'] = '#f0027f'
 
 BORDER_COLOR = 'lightgrey'
 LABEL_COLOR = 'k'
