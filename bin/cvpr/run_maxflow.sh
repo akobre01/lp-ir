268a47a1958bc4106e015dec458e32d4afc7d008
Ari Kobren <akobren@cs.umass.edu>
2017-08-28 22:35:00 -0400

    added a matcher based on google's implementation of max flow min cost in or-tools

---
+++ bin/cvpr/run_maxflow.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+#SBATCH --job-name=maxflow_single
+#SBATCH --output=logs/cvpr/run_maxflow_single.log
+#SBATCH -e logs/cvpr/run_maxflow_single.err
+#SBATCH --partition=defq    # Partition to submit to
+#
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=0-02:00         # Runtime in D-HH:MM
+#SBATCH --mem=8000    # Memory in MB per cpu allocated
+
+set -exu
+
+DATA_NAME=$1
+DATASET=$2
+LOAD=$3
+COVERAGE=$4
+ALG="maxflow"
+OUTDIR="results/${DATA_NAME}-load=${LOAD}-cov=${COVERAGE}/${ALG}/ms=0.0/"
+
+# Create output director
+mkdir -p $OUTDIR
+
+# Run the basic lp formulation of paper matching.
+python -m src.exps.run_maxflow $LOAD $COVERAGE $DATASET $OUTDIR
+
+exit
---
+++ src/matching_models/MaxFlow.py
@@ -0,0 +1,111 @@
+from ortools.graph import pywrapgraph
+
+import numpy as np
+import uuid
+
+
+class MaxFlowMinCost(object):
+    """Solves makespan paper matching using max flow min cost algorithm."""
+    def __init__(self, loads, coverages, weights, makespan=0):
+        """Initialize a makespan matcher
+
+        Args:
+            loads - a list of integers specifying the maximum number of papers
+                  for each reviewer.
+            coverages - a list of integers specifying the number of reviews per
+                 paper.
+            weights - the affinity matrix (np.array) of papers to reviewers.
+                   Rows correspond to reviewers and columns correspond to
+                   papers.
+            makespan - optional initial makespan value.
+
+        Returns:
+            initialized makespan matcher.
+        """
+        self.n_rev = np.size(weights, axis=0)
+        self.n_pap = np.size(weights, axis=1)
+        self.loads = loads
+        self.coverages = coverages
+        self.weights = weights
+        self.id = uuid.uuid4()
+        self.makespan = makespan     # the minimum allowable paper score.
+        self.solution = np.zeros((self.n_rev, self.n_pap))
+        self.solved = False
+
+        self.min_cost_flow = pywrapgraph.SimpleMinCostFlow()
+
+        self.start_inds = []
+        self.end_inds = []
+        self.caps = []
+        self.costs = []
+        self.source = self.n_rev + self.n_pap
+        self.sink = self.n_rev + self.n_pap + 1
+
+        for i in range(self.n_rev):
+            self.start_inds.append(self.source)
+            self.end_inds.append(i)
+            self.caps.append(int(loads[i]))
+            self.costs.append(0)
+
+        for i in range(self.n_rev):
+            for j in range(self.n_pap):
+                self.start_inds.append(i)
+                self.end_inds.append(self.n_rev + j)
+                self.caps.append(1)
+                self.costs.append(int(2.0 / (1.0 + self.weights[i, j])))
+        for j in range(self.n_pap):
+            self.start_inds.append(self.n_rev + j)
+            self.end_inds.append(self.sink)
+            self.caps.append(int(self.coverages[j]))
+            self.costs.append(0)
+
+        self.supplies = np.zeros(self.n_rev + self.n_pap + 2)
+        self.supplies[self.source] = int(sum(self.coverages))
+        self.supplies[self.sink] = int(-sum(self.coverages))
+
+        print(sum(self.coverages), sum(self.loads))
+
+        for i in range(len(self.start_inds)):
+            self.min_cost_flow.AddArcWithCapacityAndUnitCost(
+                self.start_inds[i], self.end_inds[i], self.caps[i],
+                self.costs[i])
+        for i in range(len(self.supplies)):
+            self.min_cost_flow.SetNodeSupply(i, int(self.supplies[i]))
+
+    def solve(self):
+        """Solve matching."""
+        if self.min_cost_flow.Solve() == self.min_cost_flow.OPTIMAL:
+            print('Total cost = ', self.min_cost_flow.OptimalCost())
+            print()
+            for arc in range(self.min_cost_flow.NumArcs()):
+                # Can ignore arcs leading out of source or into sink.
+                if self.min_cost_flow.Tail(arc) != self.source and \
+                                self.min_cost_flow.Head(arc) != self.sink:
+                    if self.min_cost_flow.Flow(arc) > 0:
+                        rev = self.min_cost_flow.Tail(arc)
+                        pap = self.min_cost_flow.Head(arc) - self.n_rev
+                        self.solution[rev, pap] = 1.0
+            self.solved = True
+        else:
+            print('There was an issue with the min cost flow input.')
+
+    def sol_as_mat(self):
+        if self.solved:
+            return self.solution
+        else:
+            raise Exception(
+                'You must have solved the model optimally or suboptimally '
+                'before calling this function.')
+
+if __name__ == '__main__':
+    costs = np.array([[.1, .9, .3],
+                      [.1, .4, .2],
+                      [.1, .14, .5],
+                      [.7, .1, .7]])
+    m = MaxFlowMinCost(np.array([3, 3, 3, 3]), np.array([4, 4, 4]), costs)
+    m.solve()
+    print(m.solution)
+    print(m.start_inds)
+    print(m.end_inds)
+    print(m.source)
+    print(m.sink)
