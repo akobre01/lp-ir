2c96f31a13f033668ba3cac4b1c3667ae6cc8343
Ari Kobren <akobren@cs.umass.edu>
2017-09-08 14:56:43 -0400

    plot expert survival now plots for lower bound and non-lower bound versions of matching

--- swarm/subjects-only/run_plot_expert_survival.sh
+++ swarm/subjects-only/run_plot_expert_survival.sh
@@ -4,9 +4,11 @@ source `pwd`/swarm/setup.sh
 
 DATA_NAME="subjects-only"
 DATASET="data/cvpr/subjects-only.npy"
-ASSIGNMENTS="${PM_ROOT}/results/${DATA_NAME}-load=${LOAD}-cov=${COVERAGE}-loadlb=${LOAD_LB}"
+ASSIGNMENTS="${PM_ROOT}/results/${DATA_NAME}-load=${LOAD}-cov=${COVERAGE}"
+ASSIGNMENTS_LB="${PM_ROOT}/results/${DATA_NAME}-load=${LOAD}-cov=${COVERAGE}-loadlb=${LOAD_LB}"
 
 # Run the basic lp formulation of paper matching.
 sbatch $PM_ROOT/bin/util/run_plot_expert_survival.sh $ASSIGNMENTS $DATASET
+sbatch $PM_ROOT/bin/util/run_plot_expert_survival.sh $ASSIGNMENTS_LB $DATASET
 
 exit
